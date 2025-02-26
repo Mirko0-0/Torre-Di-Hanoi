da1b56af2addeb1eb511347c58bd017a30e948a0
Mirko <impala.mirko@studenti.salesianisesto.it>
2025-02-26 09:24:36 +0100

    added project

---
+++ src/Pila.java
@@ -0,0 +1,49 @@
+import java.util.ArrayList;
+
+public class Pila <T>{
+    private ArrayList <T> lista;
+
+    public Pila(){
+        lista = new ArrayList<>();
+    }
+    public void push(T elemento){
+        lista.add(elemento);
+    }
+
+    public T pop(){
+        int num = lista.size();
+        if(num>0){
+            return lista.remove(num-1);
+        }
+        return null;
+    }
+
+    public T top(){
+        int num = lista.size();
+        if(num>0){
+            return lista.get(num-1);
+        }
+        return null;
+    }
+
+    public int size(){
+        return lista.size();
+    }
+
+    public ArrayList<T> getElements() {
+        return new ArrayList<>(lista);
+    }
+
+    public boolean isEmpty(){
+        return lista.isEmpty();
+    }
+
+    @Override
+    public String toString() {
+        return "Pila{" +
+                "lista=" + lista +
+                '}';
+    }
+
+
+}
