ad438b6f90d8bc06f1ecedc576af2216e32d163e
Mirko <impala.mirko@studenti.salesianisesto.it>
2025-02-28 10:22:34 +0100

    Better GUI

--- src/Colonne.java
+++ src/Colonne.java
@@ -2,8 +2,10 @@ class Colonne {
     private Pila<Disco> torre1;
     private Pila<Disco> torre2;
     private Pila<Disco> torre3;
+    private int numDischi;
 
     public Colonne(int numDischi) {
+        this.numDischi=numDischi;
         torre1 = new Pila<>(1);
         torre2 = new Pila<>(2);
         torre3 = new Pila<>(3);
@@ -40,9 +42,29 @@ class Colonne {
     }
 
     public void stampaTorri() {
+        /*
         System.out.println("Torre 1: " + torre1.getLista());
         System.out.println("Torre 2: " + torre2.getLista());
         System.out.println("Torre 3: " + torre3.getLista());
+         */
+        int tmpsz1,tmpsz2,tmpsz3;
+        for (int i = numDischi; i >= 0; i--) {
+            String s1 = " ";
+            String s2 = " ";
+            String s3 = " ";
+
+            if (i < torre1.getLista().size())
+                s1 = torre1.getLista().get(i).toString() + " ";
+            if (i < torre2.getLista().size())
+                s2 = torre2.getLista().get(i).toString() + " ";
+            if (i < torre3.getLista().size())
+                s3 = torre3.getLista().get(i).toString() + " ";
+
+            System.out.println("        "+s1 + "              " + s2 + "              " + s3);
+        }
+        System.out.println("    Colonna1    -   Colonna2    -   Colonna3");
+
+        System.out.println("size:   "+torre1.getLista().size()+"               "+torre2.getLista().size()+"               "+torre3.getLista().size());
     }
 
     public boolean isVittoria(int numDischi) {
